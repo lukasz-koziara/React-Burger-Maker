0a1e3661a1ae20efd9ef2a8fcedbe4c05fdfb680
lukasz-koziara <lukasz.koziara88@gmail.com>
2018-08-02 11:30:50 +0200

    added propTypes, static burger is now showing

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = ( props ) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top' />
+            <BurgerIngredient type='meat' />
+            <BurgerIngredient type='cheese' />
+            <BurgerIngredient type='bread-bottom' />
+        
+        </div>
+
+
+    );
+};
+
+export default burger
